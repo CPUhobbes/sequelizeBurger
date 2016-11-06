d36fafd80f7b1e20b25231b89c75a400a0861c7b
Eric Steinberg <eric@master>
2016-11-06 17:17:49 -0600

    Added Migration File

---
+++ migrations/20161106212905-add-burgers.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var models = require("../models");
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return models.Burger.bulkCreate([
+      {
+        burger_name:"Cheeseburger",
+        devoured:"0"
+      },
+      {
+        burger_name:"Hamburger",
+        devoured:"0"
+      },
+      {
+        burger_name:"Bacon Cheeseburger",
+        devoured:"0"
+      },
+      {
+        burger_name:"Veggie Burger",
+        devoured:"0"
+      },
+      {
+        burger_name:"Chicken Burger",
+        devoured:"0"
+      },
+    ]);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    var removeList = [
+        "Cheeseburger",
+        "Hamburger",
+        "Bacon Cheeseburger",
+        "Veggie Burger",
+        "Chicken Burger"
+    ];
+
+    return models.Burger.max('id')
+      .then(function(results){
+
+        return models.Burger.destroy({where:{burger_name: removeList}})
+        .then(function(recordsDestroyed){
+          
+          var newKey = results-(recordsDestroyed-1) ;
+          console.log(results, newKey);
+          var sequelize = new Sequelize('burgers_db', 'root', '', {dialect: 'mysql'});
+          return sequelize.query('ALTER TABLE Burgers AUTO_INCREMENT = '+newKey);
+      })
+    })
+  }
+};
